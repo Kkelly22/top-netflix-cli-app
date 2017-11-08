1f5a5d3924e432f13e614718cfa57d6025b921ee
Kaitlin Kelly <kk504408@gmail.com>
2017-11-08 02:17:39 +0000

    Added object oriented data structure to cli/shows

--- lib/top_netflix/cli.rb
+++ lib/top_netflix/cli.rb
@@ -7,40 +7,33 @@ class TopNetflix::CLI
   end
 
   def welcome
-    puts "Top Shows to Stream on Netflix"
-    puts "------------------------------"
+    puts "Today's Top Shows to Stream on Netflix"
+    puts "--------------------------------------"
   end
 
   def list_shows   #this will scrape our website and list each show by name each.do
     @shows = TopNetflix::Shows.Today
+    @shows.each.with_index(1) do  |show, i|
+      puts "#{i}. #{show.name}"
+    end
   end
 
   def user_choice
     input = " "
     while input != "exit"
       puts "To view more information, please choose a show (1-8). To see the options, type list. To exit, type exit."
-      input = gets.strip
+      input = gets.strip.downcase
 
       if input.to_i > 0
-        show_details(input)
+        the_show = @shows[input.to_i - 1]
+        puts "#{the_show.name} - #{the_show.release_date} - #{the_show.plot} - #{the_show.pros} - #{the_show.cons}"
       elsif input == "list"
         list_shows
       elsif input == "exit"
         puts "Happy streaming!"
       else
         puts "Not sure what you mean, try again"
-        list_shows
       end
-    end
-  end
-
-  def show_details(input)   #this will show individual shows attributes based on name
-    if input == "1"
-      puts "Season Release: Nov 3, 2017.  Pros: Cons: Plot:"
-    elsif input == "2"
-      puts "Season Release: October 31, 2017.  Pros: Cons: Plot:"
-    else
-      puts "Do not have information on that choice, try another."
     end
   end
 
--- lib/top_netflix/shows.rb
+++ lib/top_netflix/shows.rb
@@ -2,8 +2,24 @@ class TopNetflix::Shows
   attr_accessor :name, :release_date, :pros, :cons, :plot
 
   def self.Today
-    puts "1. Stranger Things"
-    puts "2. Life in Pieces"
+  #  puts "1. Stranger Things"
+  #  puts "2. Life in Pieces"
+
+    show_1 = self.new
+    show_1.name = "Stranger Things"
+    show_1.release_date = "October 31, 2017"
+    show_1.pros = "Edge of your seat sci-fi thriller"
+    show_1.cons = "Have to wait a year for each season"
+    show_1.plot = "Sci-Fi thriller about the upside down"
+
+    show_2 = self.new
+    show_2.name = "Life In Pieces"
+    show_2.release_date = "November 3, 2017"
+    show_2.pros = "Jokes that make you laugh every minute of watching"
+    show_2.cons = "Short episodes"
+    show_2.plot = "Whimsical comedy about a family unit of 4"
+
+    [show_1, show_2]
   end
 
 end
