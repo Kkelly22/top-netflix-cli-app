f1f88aa054711f862ec5e70250dafe992f900979
Kaitlin Kelly <kk504408@gmail.com>
2017-11-08 02:16:30 +0000

    Added nokogiri and open uri to environment/depend

--- lib/top_netflix.rb
+++ lib/top_netflix.rb
@@ -1,4 +1,7 @@
-require_relative "./top_netflix/version"
+require "nokogiri"
+require "pry"
+require "open-uri"
 
+require_relative "./top_netflix/version"
 require_relative "./top_netflix/cli"
 require_relative "./top_netflix/shows"
