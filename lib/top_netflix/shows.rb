40fe08bba9a9c9162085d921e49f7eb339690ee1
Kaitlin Kelly <kk504408@gmail.com>
2017-11-08 15:47:37 +0000

    Slight refactoring of scraping

--- lib/top_netflix/shows.rb
+++ lib/top_netflix/shows.rb
@@ -2,10 +2,14 @@ class TopNetflix::Shows
   attr_accessor :name, :release_date, :plot, :pros, :cons
 
   def self.Today
-    show_array = []
+    self.scrape_shows
+  end
+
+  def self.scrape_shows
     url = "https://www.huffingtonpost.com/entry/netflix-streamline_us_59f9e246e4b046017fb05974"
-    html = open(url)
-    doc = Nokogiri::HTML(html)
+    doc = Nokogiri::HTML(open(url))
+
+    show_array = []
     i = 0
     until i > 7
       show = self.new
@@ -19,22 +23,6 @@ class TopNetflix::Shows
       show_array << show
       i += 1
     end
-
-#    show_1 = self.new
-#    show_1.name = "Stranger Things"
-#    show_1.release_date = "October 31, 2017"
-#    show_1.pros = "Edge of your seat sci-fi thriller"
-#    show_1.cons = "Have to wait a year for each season"
-#    show_1.plot = "Sci-Fi thriller about the upside down"
-
-#    show_2 = self.new
-#    show_2.name = "Life In Pieces"
-#    show_2.release_date = "November 3, 2017"
-#    show_2.pros = "Jokes that make you laugh every minute of watching"
-#    show_2.cons = "Short episodes"
-#    show_2.plot = "Whimsical comedy about a family unit of 4"
-
-#    [show_1, show_2]
     show_array
   end
 
